78903622850628aea823191c82724036f011e80a
yukkop <hectic.yukkop@gmail.com>
2025-01-25 02:51:53 +0000

    fix: extend hetzner hardware module

--- flake.nix
+++ flake.nix
@@ -119,6 +119,13 @@
 
     nixosModules.${system} = {
       "hetzner.hardware" = {
+            boot.loader.grub.device = "/dev/sda";
+            boot.initrd.availableKernelModules = [
+	      "ata_piix"
+	      "uhci_hcd"
+	      "xen_blkfront"
+	      "vmw_pvscsi"
+	    ];
             boot.initrd.kernelModules = [ "nvme" ];
             fileSystems."/" = { device = "/dev/sda1"; fsType = "ext4"; };
        };
