54cc7fa9f6a877aa82808d877ea575d576a2f471
yukkop <hectic.yukkop@gmail.com>
2025-07-03 01:21:44 +0000

    feat(package): `slpt`: jq dependency

--- package/slpt.nix
+++ package/slpt.nix
@@ -1,8 +1,12 @@
-{ lib, writeShellScriptBin, fetchFromGitHub }: let
+{ lib, writeShellScriptBin, fetchFromGitHub, pkgs }: let
   src = fetchFromGitHub {
     owner = "nativerv";
     repo = "slpt";
     rev = "8d70db4d8dfcd624ed49b9e6fb0ad449b6f25b89";
     hash = "sha256-sCHZsf7Y36iAesh7BeSxy9WhE/uQv13/VWmjlaVSEcU=";
   };
-in writeShellScriptBin "slpt" (builtins.readFile "${src}/slpt")
+in writeShellScriptBin "slpt" ''
+  #!${pkgs.runtimeShell}
+  PATH=${lib.makeBinPath [ pkgs.jq ]}:$PATH
+  ${builtins.readFile "${src}/slpt"}
+''
