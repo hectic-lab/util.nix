a7fbd59144a33808fcaeb042942461ed93cbbc9e
yukkop <hectic.yukkop@gmail.com>
2025-07-24 19:24:31 +0000

    fix(package): `nix-derivation-hash`: purify

--- package/nix-derivation-hash/default.nix
+++ package/nix-derivation-hash/default.nix
@@ -1,9 +1,13 @@
 {
   writeShellScriptBin,
   bash,
+  nix,
 }: let
   # Use folder name as name of this system
   name = builtins.baseNameOf ./.;
 in writeShellScriptBin name /* sh */ ''
+  set -a
+  BIN_NIX_HASH="${nix}/bin/nix-hash"
+  set +a
   ${bash}/bin/sh ${./${name}.sh} "$@"
 ''
