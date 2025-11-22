278a8386d77914f540fc4c23376c7e78bdd713e9
yukkop <hectic.yukkop@gmail.com>
2025-11-22 11:50:42 +0000

    fix(package): `migrator`: init migration

--- test/package/migrator/test/migrate-up/run.sh
+++ test/package/migrator/test/migrate-up/run.sh
@@ -1,12 +1,12 @@
-##!/bin/dash
-#
-#HECTIC_NAMESPACE=test-migration-list
-#
-#psql "$DATABASE_URL" 'CREATE TABLE profile (
-#  id       INTEGER,
-#  username TEXT
-#)'
-#
-#migrator --db-url "$DATABASE_URL" migrate to 20251104192425-add-info-to-profile
-#
-#exit 1
+#!/bin/dash
+
+HECTIC_NAMESPACE=test-migration-list
+
+psql "$DATABASE_URL" 'CREATE TABLE profile (
+  id       INTEGER,
+  username TEXT
+)'
+
+migrator --db-url "$DATABASE_URL" migrate to 20251104192425-add-info-to-profile
+
+exit 1
