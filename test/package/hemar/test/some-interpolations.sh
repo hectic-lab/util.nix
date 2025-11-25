35b52733abea49179e99c2c28f1b724933c9c506
yukkop <hectic.yukkop@gmail.com>
2025-11-25 15:45:02 +0000

    feat(package): `hemar`: I think I was wrong

---
+++ test/package/hemar/test/some-interpolations.sh
@@ -0,0 +1,38 @@
+answer="$(printf '%s' 'text begind {[ "inn \\e\"r"-t\"ext ]}' | hemar -c)"
+
+expected="$(printf '%s' '[
+  {
+    "type": "text",
+    "value": "text begind "
+  },
+  {
+    "type": "interpolation",
+    "path": "inn \\e\"r-t\"ext"
+  }
+]')"
+
+json_diff "$answer" "$expected"
+
+[ "$(printf '%s' "$answer" | yq '.[1] | .path')" = 'inn \e"r-t"ext' ] || {
+  log error 'unexpected'
+  exit 1
+}
+
+answer="$(printf '%s' 'text begind {[ ["  "] ]}' | hemar -c)"
+
+expected="$(printf '%s' '[
+  {
+    "type": "text",
+    "value": "text begind "
+  },
+  {
+    "type": "interpolation",
+    "path": "[  ]"
+  }
+]')"
+
+json_diff "$answer" "$expected"
+
+answer="$(printf '%s' 'text begind {[ [" "\ ] ]}' | hemar -c)"
+
+json_diff "$answer" "$expected"
