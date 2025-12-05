3b506a78882241b460b071935f1dbef496021a1f
yukkop <hectic.yukkop@gmail.com>
2025-12-05 21:01:53 +0000

    fix: bfs nixosConfiguration check

--- flake.nix
+++ flake.nix
@@ -73,14 +73,16 @@
     packages.${system}         = import ./package           { inherit flake self inputs pkgs system; };
     devShells.${system}        = import ./devshell          { inherit flake self inputs pkgs system; };
     legacyPackages.${system}   = import ./legacy            { inherit flake self inputs pkgs system; };
-    nixosConfigurations        =                            {
-      "bfs|${system}"         = import ./nixos/system/bfs   { inherit flake self inputs system; };
-    };
+    nixosConfigurations        = {};  
     checks.${system}           = import ./test              { inherit flake self inputs pkgs system; };
   }) // {
     lib = self-lib;
     overlays.default           = import ./overlay           { inherit flake self inputs; };
     nixosModules               = import ./nixos/module      { inherit flake self inputs; };
     templates                  = import ./template          { inherit flake self inputs; };
+    nixosConfigurations
+      # NOTE(yukkop): in bfs one of dependencies is shadow-4.17.4 that
+      # unsupported on aarch64-darwin
+      ."bfs|x86_64-linux"      = import ./nixos/system/bfs  { inherit flake self inputs; system = "x86_64-linux"; };
   };
 }
