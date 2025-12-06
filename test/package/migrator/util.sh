f1337db536f5b33389b0060efdc3c60c413501ba
yukkop <hectic.yukkop@gmail.com>
2025-12-06 04:47:31 +0000

    fix(package): `migrator`: index_of subshell issue

--- test/package/migrator/util.sh
+++ test/package/migrator/util.sh
@@ -6,3 +6,15 @@ columns() {
   FROM information_schema.columns
   WHERE table_name = '"${1};"
 }
+
+# is_number(var)
+is_number() {
+  case "$1" in
+    *[!0-9]*)
+      return 1
+      ;;
+    *)
+      return 0
+      ;;
+  esac
+}
