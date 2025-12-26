678e5bb828fc40e40f2777c4586ab3aec44af221
yukkop <hectic.yukkop@gmail.com>
2025-12-26 20:22:03 +0000

    test: impruve tests

--- test/package/default.nix
+++ test/package/default.nix
@@ -1,3 +1,12 @@
 { system, inputs, self, pkgs }:   
-  (import ./migrator { inherit system inputs self pkgs; }) //
-  (import ./hemar    { inherit system inputs self pkgs; })
+let
+  prefixAttrs = prefix: set:
+    pkgs.lib.mapAttrs'
+      (name: value: {
+        name = "${prefix}${name}";
+        inherit value;
+      })
+      set;
+in
+  (prefixAttrs "migrator-" (import ./migrator { inherit system inputs self pkgs; })) //
+  (prefixAttrs "hemar-"    (import ./hemar    { inherit system inputs self pkgs; }))
