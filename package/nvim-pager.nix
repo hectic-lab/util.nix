4be09990d1126acf6b7a059c83721a30b08109ba
yukkop <hectic.yukkop@gmail.com>
2026-02-17 08:55:41 +0000

    fix: nvim-pager close all pages on q

--- package/nvim-pager.nix
+++ package/nvim-pager.nix
@@ -1,7 +1,7 @@
 {pkgs, ...}:
 pkgs.writeShellScriptBin "pager" ''
   nvim -R -u NONE \
-       -c 'nnoremap q :q!<CR>' \
+       -c 'nnoremap q :qa!<CR>' \
        -c 'set buftype=nofile nowrap' \
        -c 'set runtimepath^=${pkgs.vimPlugins.vim-plugin-AnsiEsc}' \
        -c 'runtime! plugin/*.vim' \
