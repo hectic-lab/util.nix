e16f6add775c45890c0ba0b971c0ad45c6438211
yukkop <hectic.yukkop@gmail.com>
2026-05-27 12:54:15 +0000

    feat: `matrix-cluster`: shared users

--- nixos/module/generic/matrix-cluster-users.nix
+++ nixos/module/generic/matrix-cluster-users.nix
@@ -3,46 +3,40 @@
   flake,
   self,
 }: {
+  lib,
   config,
   ...
-}: {
-  hectic.generic.matrix-cluster.users = {
-    yukkop = {
-      passwordFile = config.sops.secrets."matrix/users/yukkop/password".path;
-      admin = true;
-    };
-    liquiz = {
-      passwordFile = config.sops.secrets."matrix/users/liquiz/password".path;
-    };
-    vismajor = {
-      passwordFile = config.sops.secrets."matrix/users/vismajor/password".path;
-    };
-    lvgkcfjl = {
-      passwordFile = config.sops.secrets."matrix/users/lvgkcfjl/password".path;
-    };
-  };
+}: let
+  userNames = [
+    "yukkop"
+    "liquiz"
+    "vismajor"
+    "lvgkcfjl"
+    "MrAlex0O"
+    "Антоша"
+  ];
 
-  sops.secrets."matrix/users/yukkop/password" = {
-    key      = "matrix/users/yukkop/password";
-    owner    = "matrix-synapse";
-    sopsFile = "${flake}/sus/matrix-cluster.yaml";
-  };
+  adminNames = [ "yukkop" ];
+in {
+  hectic.generic.matrix-cluster.users = builtins.listToAttrs (
+    map (name: {
+      inherit name;
+      value = {
+        passwordFile = config.sops.secrets."matrix/users/${name}/password".path;
+      } // lib.optionalAttrs (builtins.elem name adminNames) {
+        admin = true;
+      };
+    }) userNames
+  );
 
-  sops.secrets."matrix/users/liquiz/password" = {
-    key      = "matrix/users/liquiz/password";
-    owner    = "matrix-synapse";
-    sopsFile = "${flake}/sus/matrix-cluster.yaml";
-  };
-
-  sops.secrets."matrix/users/vismajor/password" = {
-    key      = "matrix/users/vismajor/password";
-    owner    = "matrix-synapse";
-    sopsFile = "${flake}/sus/matrix-cluster.yaml";
-  };
-
-  sops.secrets."matrix/users/lvgkcfjl/password" = {
-    key      = "matrix/users/lvgkcfjl/password";
-    owner    = "matrix-synapse";
-    sopsFile = "${flake}/sus/matrix-cluster.yaml";
-  };
+  sops.secrets = builtins.listToAttrs (
+    map (name: {
+      name = "matrix/users/${name}/password";
+      value = {
+        key      = "matrix/users/${name}/password";
+        owner    = "matrix-synapse";
+        sopsFile = "${flake}/sus/matrix-cluster.yaml";
+      };
+    }) userNames
+  );
 }
